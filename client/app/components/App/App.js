b0740bb2bc45d0fec34a395596890119bad78ce6
myusername <myemail>
2021-11-18 03:08:46 +0100

    Login_MERN 0.0.0

---
+++ client/app/components/App/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import Header from '../Header/Header';
+const App = ({ children }) => (
+  <>
+    <Header />
+
+    <main>
+      {children}
+    </main>
+
+  
+  </>
+);
+
+export default App;
---
+++ client/app/components/ToDoList/Todolist.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// import './style.css'
+
+const ToDoList = () => (
+    <body>
+        <p>p</p>
+         </body>
+);
+
+export default ToDoList;
